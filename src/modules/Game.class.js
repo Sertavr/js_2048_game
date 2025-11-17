c74a4320772be101361ad86d06aa99bc9b8d594e
Sertavr <pampuh.net@gmail.com>
2025-11-17 19:14:46 +0200

    added task solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -5,7 +5,7 @@
  * Now it has a basic structure, that is needed for testing.
  * Feel free to add more props and methods if needed.
  */
-class Game {
+export default class Game {
   /**
    * Creates a new game instance.
    *
@@ -20,25 +20,341 @@ class Game {
    * If passed, the board will be initialized with the provided
    * initial state.
    */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+  initialState = [
+    Array.from({ length: 4 }, (el) => ''),
+    Array.from({ length: 4 }, (el) => ''),
+    Array.from({ length: 4 }, (el) => ''),
+    Array.from({ length: 4 }, (el) => ''),
+  ];
+  score = 0;
+  status = 'idle';
+  isAlowedStep = ['', '', '', ''];
+  constructor(scoreContainer, container, currentState = this.initialState) {
+    this.currentState = JSON.parse(JSON.stringify(currentState));
+    this.tBodyEl = container.querySelector('tbody');
+    this.scoreContainer = scoreContainer;
+    this.messages = container.querySelectorAll('.message');
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    document.addEventListener('keydown', (e) => {
+      if (e.key !== 'ArrowLeft') {
+        return;
+      }
+
+      if (this.status === 'win') {
+        return;
+      }
+
+      if (
+        this.isAlowedStep.length === 4 &&
+        this.isAlowedStep.every((el) => el === false)
+      ) {
+        this.youLose();
+      }
+
+      const copyState = JSON.parse(JSON.stringify(this.currentState));
+      let isDifferent = false;
+
+      copyState.forEach((row, ind) => {
+        // const modifyArr = row.filter((el) => el !== '');
+        // const sumArr = [];
+        // let isSame = false;
+
+        // for (let i = 0; i < modifyArr.length; i++) {
+        //   if (isSame) {
+        //     isSame = false;
+        //     continue;
+        //   }
+
+        //   if (modifyArr[i] === modifyArr[i + 1]) {
+        //     isSame = true;
+        //     sumArr.push(+modifyArr[i] * 2);
+        //   } else {
+        //     sumArr.push(+modifyArr[i]);
+        //   }
+        // }
+
+        // const alignLeft = [...sumArr, ...Array(4 - sumArr.length).fill('')];
+
+        const alignLeft = this.moveLeftUp(row);
+
+        if (!isDifferent) {
+          alignLeft.forEach((el, index) => {
+            if (el !== row[index]) {
+              isDifferent = true;
+            }
+          });
+        }
+
+        copyState[ind] = alignLeft;
+      });
+
+      this.isAlowedStep[0] = isDifferent;
+
+      if (isDifferent) {
+        this.isAlowedStep = ['', '', '', ''];
+
+        const cellCoordOne = this.getRandomCell(copyState);
+        const [rowOne, columnOne] = cellCoordOne;
+
+        copyState[rowOne][columnOne] = this.getRandomNumber();
+
+        this.currentState = copyState;
+        this.updateTable();
+      }
+    });
+  }
+  moveRight() {
+    document.addEventListener('keydown', (e) => {
+      if (e.key !== 'ArrowRight') {
+        return;
+      }
+
+      if (this.status === 'win') {
+        return;
+      }
+
+      if (
+        this.isAlowedStep.length === 4 &&
+        this.isAlowedStep.every((el) => el === false)
+      ) {
+        this.youLose();
+      }
+
+      const copyState = JSON.parse(JSON.stringify(this.currentState));
+      let isDifferent = false;
+
+      copyState.forEach((row, ind) => {
+        // const modifyArr = row.filter((el) => el !== '');
+        // const sumArr = [];
+        // let isSame = false;
+
+        // for (let i = modifyArr.length - 1; i >= 0; i--) {
+        //   if (isSame) {
+        //     isSame = false;
+        //     continue;
+        //   }
+
+        //   if (modifyArr[i] === modifyArr[i - 1]) {
+        //     isSame = true;
+        //     sumArr.unshift(+modifyArr[i] * 2);
+        //   } else {
+        //     sumArr.unshift(+modifyArr[i]);
+        //   }
+        // }
+
+        // const alignLeft = [...Array(4 - sumArr.length).fill(''), ...sumArr];
+
+        const alignLeft = this.moveRightDown(row);
+
+        if (!isDifferent) {
+          alignLeft.forEach((el, index) => {
+            if (el !== row[index]) {
+              isDifferent = true;
+            }
+          });
+        }
+
+        copyState[ind] = alignLeft;
+      });
+
+      this.isAlowedStep[1] = isDifferent;
+
+      if (isDifferent) {
+        this.isAlowedStep = ['', '', '', ''];
+
+        const cellCoordOne = this.getRandomCell(copyState);
+        const [rowOne, columnOne] = cellCoordOne;
+
+        copyState[rowOne][columnOne] = this.getRandomNumber();
+
+        this.currentState = copyState;
+        this.updateTable();
+      }
+    });
+  }
+  moveUp() {
+    document.addEventListener('keydown', (e) => {
+      if (e.key !== 'ArrowUp') {
+        return;
+      }
+
+      if (this.status === 'win') {
+        return;
+      }
+
+      if (
+        this.isAlowedStep.length === 4 &&
+        this.isAlowedStep.every((el) => el === false)
+      ) {
+        this.youLose();
+      }
+
+      const copyState = JSON.parse(JSON.stringify(this.currentState));
+      let isDifferent = false;
+
+      for (let i = 0; i < copyState.length; i++) {
+        const column = [];
+
+        copyState.forEach((row) => {
+          column.push(row[i]);
+        });
+
+        const afterMove = this.moveLeftUp(column);
+
+        afterMove.forEach((el, ind) => {
+          copyState[ind][i] = el;
+        });
+
+        if (!isDifferent) {
+          afterMove.forEach((el, index) => {
+            if (el !== column[index]) {
+              isDifferent = true;
+            }
+          });
+        }
+      }
+
+      this.isAlowedStep[2] = isDifferent;
+
+      if (isDifferent) {
+        this.isAlowedStep = ['', '', '', ''];
+
+        const cellCoordOne = this.getRandomCell(copyState);
+        const [rowOne, columnOne] = cellCoordOne;
+
+        copyState[rowOne][columnOne] = this.getRandomNumber();
+
+        this.currentState = copyState;
+        this.updateTable();
+      }
+    });
+  }
+  moveDown() {
+    document.addEventListener('keydown', (e) => {
+      if (e.key !== 'ArrowDown') {
+        return;
+      }
+
+      if (this.status === 'win') {
+        return;
+      }
+
+      if (
+        this.isAlowedStep.length === 4 &&
+        this.isAlowedStep.every((el) => el === false)
+      ) {
+        this.youLose();
+      }
+
+      const copyState = JSON.parse(JSON.stringify(this.currentState));
+      let isDifferent = false;
+
+      for (let i = 0; i < copyState.length; i++) {
+        const column = [];
+
+        copyState.forEach((row) => {
+          column.push(row[i]);
+        });
+
+        const afterMove = this.moveRightDown(column);
+
+        afterMove.forEach((el, ind) => {
+          copyState[ind][i] = el;
+        });
+
+        if (!isDifferent) {
+          afterMove.forEach((el, index) => {
+            if (el !== column[index]) {
+              isDifferent = true;
+            }
+          });
+        }
+      }
+
+      this.isAlowedStep[3] = isDifferent;
+
+      if (isDifferent) {
+        this.isAlowedStep = ['', '', '', ''];
+
+        const cellCoordOne = this.getRandomCell(copyState);
+        const [rowOne, columnOne] = cellCoordOne;
+
+        copyState[rowOne][columnOne] = this.getRandomNumber();
+
+        this.currentState = copyState;
+        this.updateTable();
+      }
+    });
+  }
+
+  moveRightDown(arr) {
+    const newArr = arr.filter((el) => el !== '');
+
+    const sumArr = [];
+    let isSame = false;
+
+    for (let i = newArr.length - 1; i >= 0; i--) {
+      if (isSame) {
+        isSame = false;
+        continue;
+      }
+
+      if (newArr[i] === newArr[i - 1]) {
+        isSame = true;
+        sumArr.unshift(+newArr[i] * 2);
+        this.score += +newArr[i] * 2;
+        this.updateScore(this.scoreContainer);
+      } else {
+        sumArr.unshift(+newArr[i]);
+      }
+    }
+
+    const result = [...Array(4 - sumArr.length).fill(''), ...sumArr];
+
+    return result;
+  }
+
+  moveLeftUp(arr) {
+    const modifyArr = arr.filter((el) => el !== '');
+    const sumArr = [];
+    let isSame = false;
+
+    for (let i = 0; i < modifyArr.length; i++) {
+      if (isSame) {
+        isSame = false;
+        continue;
+      }
+
+      if (modifyArr[i] === modifyArr[i + 1]) {
+        isSame = true;
+        sumArr.push(+modifyArr[i] * 2);
+        this.score += +modifyArr[i] * 2;
+        this.updateScore(this.scoreContainer);
+      } else {
+        sumArr.push(+modifyArr[i]);
+      }
+    }
+
+    const result = [...sumArr, ...Array(4 - sumArr.length).fill('')];
+
+    return result;
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return this.score;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.currentState;
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +366,116 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    return this.status;
+  }
+
+  set statusGame(value) {
+    this.status = value;
+  }
+
+  get statusGame() {
+    return this.status;
+  }
 
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    const copyState = JSON.parse(JSON.stringify(this.currentState));
+    const cellCoordOne = this.getRandomCell(copyState);
+    const [rowOne, columnOne] = cellCoordOne;
+
+    copyState[rowOne][columnOne] = this.getRandomNumber();
+
+    const cellCoordTwo = this.getRandomCell(copyState);
+    const [rowTwo, columnTwo] = cellCoordTwo;
+
+    copyState[rowTwo][columnTwo] = this.getRandomNumber();
+    this.currentState = JSON.parse(JSON.stringify(copyState));
+
+    this.updateTable();
+    this.moveLeft();
+    this.moveRight();
+    this.moveDown();
+    this.moveUp();
+    this.status = `playing`;
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    this.currentState = this.initialState;
+    this.updateTable();
+  }
 
-  // Add your own methods here
+  getRandomNumber() {
+    const randomNum = Math.random();
+    const num = randomNum < 0.1 ? 4 : 2;
+
+    return num;
+  }
+
+  updateTable() {
+    const rowsCollection = this.tBodyEl.querySelectorAll('tr');
+
+    rowsCollection.forEach((row, ind) => {
+      const tdCollection = row.querySelectorAll('td');
+
+      tdCollection.forEach((td, i) => {
+        const num = this.currentState[ind][i];
+        const className = td.className
+          .split(' ')
+          .find((el) => el.includes('field-cell--'));
+
+        td.classList.remove(className);
+        td.textContent = num;
+
+        if (num) {
+          td.classList.add(`field-cell--${num}`);
+        }
+      });
+    });
+  }
+
+  updateScore() {
+    this.scoreContainer.textContent = this.score;
+  }
+
+  // повертає маcив координат нової клітинки
+  getRandomCell(state) {
+    let isAvailable = false;
+
+    state.forEach((row, i) => {
+      const control = row.includes('');
+
+      if (control) {
+        isAvailable = true;
+      }
+    });
+
+    if (isAvailable) {
+      while (true) {
+        const row = Math.trunc(Math.random() * 4);
+        const col = Math.trunc(Math.random() * 4);
+
+        if (state[row][col] === '') {
+          return [row, col];
+        }
+      }
+    }
+  }
+
+  youLose() {
+    this.status = 'lose';
+
+    this.messages.forEach((message) => {
+      if (message.classList.contains('message-lose')) {
+        message.classList.remove('hidden');
+      }
+    });
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,57 @@
-'use strict';
+import Game from '../modules/Game.class';
 
 // Uncomment the next lines to use your game instance in the browser
 // const Game = require('../modules/Game.class');
 // const game = new Game();
 
-// Write your code here
+const container = document.querySelector('.container');
+const tBodyEl = document.querySelector('tbody');
+const button = document.querySelector('.button');
+const score = document.querySelector('.game-score');
+const messagesCollection = document.querySelectorAll('.message');
+const game = new Game(score, container);
+
+button.addEventListener('click', function () {
+  if (this.classList.contains('start')) {
+    game.start();
+    this.classList.replace('start', 'restart');
+    this.textContent = 'Restart';
+
+    messagesCollection.forEach((message) => {
+      message.classList.add('hidden');
+    });
+  } else {
+    game.restart();
+    this.classList.replace('restart', 'start');
+    this.textContent = 'Start';
+
+    messagesCollection.forEach((message) => {
+      if (message.classList.contains('message-start')) {
+        message.classList.remove('hidden');
+      } else {
+        message.classList.add('hidden');
+      }
+    });
+  }
+  score.textContent = game.score;
+});
+
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.target.textContent === '2048') {
+      game.statusGame = `win`;
+
+      messagesCollection.forEach((message) => {
+        if (message.classList.contains('message-win')) {
+          message.classList.remove('hidden');
+        }
+      });
+    }
+  });
+});
+
+observer.observe(tBodyEl, {
+  childList: true, // слухає додавання/видалення вузлів
+  characterData: true, // слухає зміну тексту
+  subtree: true, // слухає всі вкладені елементи (td, tr, span...)
+});
